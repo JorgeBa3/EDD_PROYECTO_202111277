f244c7fdbee6afab369a3dded80603610ec1178e
JorgeBa3 <49853820+JorgeBa3@users.noreply.github.com>
2024-03-22 00:41:43 -0600

    Registro usuario e inicio de sesion

--- Fase2/app/main.f90
+++ Fase2/app/main.f90
@@ -1,49 +1,117 @@
-program main
-  use matrix_m
+program MenuPrincipal
+  use linked_list_m
+
   implicit none
+  type(linked_list):: lista_usuarios
+  character(len=20) :: usuario, password
+  integer :: opcion
+
+  ! Datos del usuario administrador
+  character(len=20) :: usuario_admin = 'admin'
+  character(len=20) :: password_admin = 'EDD2024'
+
+  ! Variables para el registro de usuarios
+  character(len=20) :: nombre_completo, dpi, password_usuario
+
+  ! Variables para controlar el inicio de sesión
+  logical :: sesion_iniciada
+  sesion_iniciada = .false.
+
+  do
+      print *, "---- MENÚ PRINCIPAL ----"
+      print *, "1. Inicio de Sesión"
+      print *, "2. Registro de Usuarios"
+      print *, "3. Acerca de"
+      print *, "4. Salir"
+      print *, "Seleccione una opción: "
+      read(*,*) opcion
+
+      select case(opcion)
+      case(1)
+          call IniciarSesion(usuario, password, sesion_iniciada, lista_usuarios)
+      case(2)
+          call RegistrarUsuario(nombre_completo, dpi, password_usuario)
+      case(3)
+          call AcercaDe
+      case(4)
+          exit
+      case default
+          print *, "Opción no válida. Por favor, seleccione una opción válida."
+  end select
   
-  type(matrix) :: m
+  end do
 
-  call m%insert(1, 14, "#000000")
-  call m%insert(2, 14, "#000000")
-  
-  call m%insert(4, 14, "#000000")
+contains
 
-  call m%insert(1, 15, "#000000")
-  call m%insert(3, 15, "#000000")
-  call m%insert(4, 15, "#000000")
+subroutine IniciarSesion(usuario, password, sesion_iniciada, lista_usuarios)
+  use linked_list_m
+  implicit none
+  character(len=20), intent(out) :: usuario
+  character(len=20), intent(out) :: password
+  logical, intent(out) :: sesion_iniciada
+  class(linked_list), intent(in) :: lista_usuarios
 
-  call m%insert(1, 16, "#000000")
-  call m%insert(4, 16, "#000000")
+  character(len=5) :: usuario_admin = 'admin'
+  character(len=10) :: password_admin = 'EDD2024'
 
-  call m%insert(1, 17, "#000000")
-  call m%insert(4, 17, "#000000")
+  type(user), pointer :: current_user
 
-  call m%insert(2, 18, "#000000")
-  call m%insert(4, 18, "#000000")
+  ! Lógica para el inicio de sesión
+  print *, "Ingrese el nombre de usuario: "
+  read(*,*) usuario
+  print *, "Ingrese la contraseña: "
+  read(*,*) password
 
-  call m%insert(2, 19, "#000000")
+  sesion_iniciada = .false. ! Por defecto, la sesión no está iniciada
 
-  call m%insert(3, 20, "#000000")
+  ! Comprobamos si el usuario es el administrador
+  if (usuario == usuario_admin .and. password == password_admin) then
+      print *, "Inicio de sesión exitoso como administrador."
+      sesion_iniciada = .true.
+      return
+  end if
 
-  call m%insert(3, 21, "#000000")
+  ! Si el usuario no es el administrador, buscamos en la lista de usuarios registrados
+  current_user => lista_usuarios%get_head() ! Using get_head method to get the head of the linked list
+  do while (associated(current_user))
+    
+      if (trim(current_user%nombre_completo) == trim(usuario) .and. &
+          trim(current_user%contrasena) == trim(password)) then
+          print *, "Inicio de sesión exitoso como ", trim(current_user%nombre_completo), " (usuario)."
+          sesion_iniciada = .true.
+          return
+      end if
+      current_user => current_user%next
+  end do
 
-  call m%insert(4, 22, "#000000")
+  ! Si no se encontró el usuario en la lista, se muestra un mensaje de error
+  print *, "Nombre de usuario o contraseña incorrectos."
+end subroutine IniciarSesion
 
-  call m%insert(5, 10, "#000000")
+  subroutine RegistrarUsuario(nombre_completo, dpi, password_usuario)
+      implicit none
+      character(len=20), intent(out) :: nombre_completo
+      character(len=20), intent(out) :: dpi
+      character(len=20), intent(out) :: password_usuario
 
-  call m%insert(5, 11, "#000000")
+      ! Lógica para el registro de usuarios
+      print *, "Ingrese el nombre completo del nuevo usuario: "
+      read(*,*) nombre_completo
+      print *, "Ingrese el DPI del nuevo usuario: "
+      read(*,*) dpi
+      print *, "Ingrese la contraseña para el nuevo usuario: "
+      read(*,*) password_usuario
 
-  call m%insert(5, 16, "#000000")
+      ! Agregar el usuario a la lista enlazada
+    call lista_usuarios%append_usuario(nombre_completo, dpi, password_usuario)
 
-  call m%insert(5, 17, "#000000")
+  end subroutine RegistrarUsuario
 
-  call m%insert(5, 19, "#000000")
+  subroutine AcercaDe
+      implicit none
 
-  call m%insert(5, 20, "#000000")
+      ! Información acerca del programa
+      print *, "Este es un programa de ejemplo para un menú en Fortran."
+  end subroutine AcercaDe
 
-  call m%insert(5, 23, "#000000")
-  
-  call m%graficar()
-  call m%print()
-end program main
+end program MenuPrincipal
---
+++ Fase2/src/lista_usuarios.f90
@@ -0,0 +1,112 @@
+module linked_list_m
+    implicit none
+    private
+
+    public :: user
+    public :: linked_list
+    public :: push_usuario, append_usuario, print_users, destructor, get_head
+    
+    type :: user
+
+        
+        character(len=20) :: nombre_completo
+        character(len=20) :: dpi
+        character(len=20) :: contrasena
+        type(user), pointer :: next => null()
+    end type user
+
+    type, public :: linked_list
+        private
+        type(user), pointer :: head => null()
+
+    contains
+        procedure :: push_usuario
+        procedure :: append_usuario
+        ! procedure :: insert
+        procedure :: print_users
+        final :: destructor
+        procedure :: get_head ! Método para obtener el head de la lista enlazada
+    end type linked_list
+
+contains
+    subroutine push_usuario(self, nombre_completo, dpi, contrasena)
+        class(linked_list), intent(inout) :: self
+        character(len=20), intent(in) :: nombre_completo
+        character(len=20), intent(in) :: dpi
+        character(len=20), intent(in) :: contrasena
+
+        type(user), pointer :: new_user
+        allocate(new_user)
+
+        new_user%nombre_completo = nombre_completo
+        new_user%dpi = dpi
+        new_user%contrasena = contrasena
+
+        if(.not. associated(self%head)) then
+            self%head => new_user
+        else    
+            new_user%next => self%head
+            self%head => new_user
+        end if
+    end subroutine push_usuario
+
+    subroutine append_usuario(self, nombre_completo, dpi, contrasena)
+        class(linked_list), intent(inout) :: self
+        character(len=20), intent(in) :: nombre_completo
+        character(len=20), intent(in) :: dpi
+        character(len=20), intent(in) :: contrasena
+
+        type(user), pointer :: current_user
+        type(user), pointer :: new_user
+        allocate(new_user)
+
+        new_user%nombre_completo = nombre_completo
+        new_user%dpi = dpi
+        new_user%contrasena = contrasena
+
+        if(.not. associated(self%head)) then
+            self%head => new_user
+        else
+            current_user => self%head
+            do while(associated(current_user%next))
+                current_user => current_user%next
+            end do
+
+            current_user%next => new_user
+        end if
+
+    end subroutine append_usuario
+
+    subroutine print_users(self)
+        class(linked_list), intent(in) :: self
+        type(user), pointer :: current_user
+        current_user => self%head
+
+        do while(associated(current_user))
+            print *, "Nombre completo: ", trim(current_user%nombre_completo)
+            print *, "DPI: ", trim(current_user%dpi)
+            print *, "Contraseña: ", trim(current_user%contrasena)
+            print *
+            current_user => current_user%next
+        end do
+    end subroutine print_users
+
+    subroutine destructor(self)
+        type(linked_list), intent(inout) :: self
+        type(user), pointer :: aux_user
+
+        do while(associated(self%head))
+            aux_user => self%head%next
+            deallocate(self%head)
+            self%head = aux_user
+        end do
+    end subroutine destructor
+
+    function get_head(self) result(head_ptr)
+        class(linked_list), intent(in) :: self
+        type(user), pointer :: head_ptr
+
+        head_ptr => self%head
+    end function get_head
+
+end module linked_list_m
